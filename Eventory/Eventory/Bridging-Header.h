b501060b1e45b72fb1aa74b365612e9bf58a2b4e
Imran Ahmed <96imranahmed@gmail.com>
2015-07-05 00:06:31 +0100

    Removed CocoaPods - restarted project

---
+++ Eventory/Eventory/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  Eventory
+//
+//  Created by Imran Ahmed on 05/07/2015.
+//  Copyright (c) 2015 Imran Ahmed. All rights reserved.
+//
+
+#ifndef Eventory_Bridging_Header_h
+#define Eventory_Bridging_Header_h
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#endif
